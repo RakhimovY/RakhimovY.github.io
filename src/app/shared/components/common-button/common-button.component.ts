fb644442271ca46eb79be1fd61d3b7d94993209b
RakhimovY <113960603+RakhimovY@users.noreply.github.com>
2024-06-07 08:40:14 +0500

    init() : footer

---
+++ src/app/shared/components/common-button/common-button.component.ts
@@ -0,0 +1,26 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-common-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './common-button.component.html',
+  styleUrl: './common-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommonButtonComponent {
+  @Input() text!: string;
+  @Input() type: 'primary' = 'primary';
+  @Output() clickBTN: EventEmitter<any> = new EventEmitter();
+
+  clickBtn() {
+    this.clickBTN.emit();
+  }
+}
