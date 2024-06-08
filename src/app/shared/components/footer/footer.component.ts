26da518e043d8d42479d6e102c470a5c777af79f
YR-F <erke.bulan622@gmail.com>
2024-06-08 16:10:15 +0500

    feat(footer) : footer done

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonButtonComponent } from '../common-button/common-button.component';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule, CommonButtonComponent],
+  imports: [TranslateModule, CommonButtonComponent, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
