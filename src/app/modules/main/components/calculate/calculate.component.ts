ca26e6178a75c90737fa8fcd0a0a158fd131ea36
00057754 <ErkebulanR@halykbank.kz>
2024-06-19 14:48:05 +0500

    feat(calculate) : добил calculate и добавил preview
    feat(calculate) : добил calculate и добавил preview

--- src/app/modules/main/components/calculate/calculate.component.ts
+++ src/app/modules/main/components/calculate/calculate.component.ts
@@ -5,21 +5,39 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { tap } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-calculate',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './calculate.component.html',
   styleUrl: './calculate.component.scss',
 })
 export class CalculateComponent implements OnInit {
-  rateControl = new FormControl('', [Validators.max(100), Validators.min(0)]);
+  rateControl = new FormControl('0', [Validators.max(100), Validators.min(0)]);
+  isDecrementBtnDisabled$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  isIncrementBtnDisabled$: BehaviorSubject<boolean> = new BehaviorSubject(
+    false
+  );
 
   ngOnInit(): void {
     this.rateControl.valueChanges
       .pipe(
+        tap((value) => {
+          if (value !== undefined && value !== null) {
+            +value <= 0
+              ? this.isDecrementBtnDisabled$.next(true)
+              : this.isDecrementBtnDisabled$.next(false);
+
+            +value >= 100
+              ? this.isIncrementBtnDisabled$.next(true)
+              : this.isIncrementBtnDisabled$.next(false);
+          } else {
+            this.rateControl.setValue('0');
+          }
+        }),
         tap((value) => {
           if (typeof value === 'number') {
             value > 100
@@ -31,5 +49,21 @@ export class CalculateComponent implements OnInit {
         })
       )
       .subscribe();
+  }
+
+  onIncrement() {
+    if (this.rateControl.value) {
+      this.rateControl.setValue((+this.rateControl.value + 1).toString());
+    } else {
+      this.rateControl.setValue('0');
+    }
+  }
+
+  onDecrement() {
+    if (this.rateControl.value) {
+      this.rateControl.setValue((+this.rateControl.value - 1).toString());
+    } else {
+      this.rateControl.setValue('0');
+    }
   }
 }
--- src/app/modules/main/components/preview/preview.component.ts
+++ src/app/modules/main/components/preview/preview.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonButtonComponent } from '../../../../shared/components/common-button/common-button.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-preview',
@@ -9,4 +10,6 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.scss',
 })
-export class PreviewComponent {}
+export class PreviewComponent {
+  protected readonly faTelegram = faTelegram;
+}
