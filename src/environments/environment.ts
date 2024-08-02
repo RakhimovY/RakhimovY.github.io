26459853b33c12c64b4865e6d565237d6afd707c
RakhimovY <rakhimov.y.hh@gmail.com>
2024-08-02 18:10:35 +0500

    Revert "feat(proxy): добавил прокси"
    This reverts commit 9e2927e832c0cfa175d3d7720d1c9cb73220eb72.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  authAPI: '/api/auth/',
-  userAPI: '/api/user/',
-  productAPI: '/api/product/',
-  adminAPI: '/api/admin/',
-  infoAPI: '/api/info/',
+  baseHost: 'https://94.247.129.77:8080/',
+  authAPI: 'https://94.247.129.77:8080/auth/',
+  userAPI: 'https://94.247.129.77:8080/user/',
+  productAPI: 'https://94.247.129.77:8080/product/',
+  adminAPI: 'https://94.247.129.77:8080/admin/',
+  infoAPI: 'https://94.247.129.77:8080/info/',
 };
